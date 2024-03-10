3cc4a045a28d4c8daf1b63f20427fa6fcdcee808
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2024-03-10 21:10:32 +0000

    refactor: move canny edge detection outside of get_angles function so that main and param_tester code can share the same function

--- main.py
+++ main.py
@@ -9,40 +9,48 @@ import utils
 
 def testTask1(folderName: str) -> int:
     dataset = pd.read_csv(f"{folderName}/list.txt")
-    pred_angles = []
     total_error = 0
 
+    gauss_kernel_size  = task1.PARAMS.canny_gauss_kernel_size
+    gauss_sigma = task1.PARAMS.canny_gauss_sigma
+    gauss_low_threshold = task1.PARAMS.canny_gauss_low_threshold
+    gauss_high_threshold = task1.PARAMS.canny_gauss_high_threshold
+
     print("Precalculate edges for each image...") 
-    images = []
+    images_edges = []
     for row in dataset.itertuples():
         filename, correct_answer = row.FileName, row.AngleInDegrees
         img = cv2.imread(f"Task1Dataset/{filename}", cv2.IMREAD_GRAYSCALE)
-        edges = utils.canny(img, gauss_kernel_size=25, sigma=2, low_threshold=70, high_threshold=110)
-        images.append((edges, correct_answer))
+        edges = utils.canny(img, gauss_kernel_size, gauss_sigma, 
+                            gauss_low_threshold, gauss_high_threshold)
+        images_edges.append((edges, correct_answer))
 
     # Just testing parameters for now
-    if True:
+    if False:
         # Adding step to everything because we don't want to include the first 
         #   elem (0) and we want to include last elem. It becomes (0, end].
-        rhos = np.arange(0, 2.5, 0.5) + 0.5
-        thetas = np.arange(0, 2.5, 0.5) + 0.5
-        thresholds = np.arange(0, 200, 40) + 40
+        #rhos = np.arange(0, 0.5, 0.05) + 0.05
+        rhos = np.array([0.1])
+        thetas = np.arange(1.65, 1.68, 0.001) + 0.001
+        thresholds = np.arange(118, 121, 0.1) + 0.1
 
-        task1.try_params(images, rhos, thetas, thresholds)
+        task1.try_params(images_edges, rhos, thetas, thresholds)
     else:
-        for row in dataset.itertuples():
-            filename, correct_answer = row.FileName, row.AngleInDegrees
-
-            # Read in image, importantly with intensity values 0-255 not 0-1
-            img = cv2.imread(f"{folderName}/{filename}", cv2.IMREAD_GRAYSCALE)
-            angle = task1.getAngleBetweenLines(img)
-            pred_angles.append(angle)
-
+        hough_threshold = task1.PARAMS.hough_threshold
+        hough_theta_res = task1.PARAMS.hough_theta_res
+        hough_rho_res = task1.PARAMS.hough_rho_res
+        
+        results = np.zeros((2, len(images_edges)))
+        for (img_index, (image, correct_answer)) in enumerate(images_edges):
+            angle = task1.get_angle_between_lines(image, hough_threshold,
+                                                  hough_theta_res, hough_rho_res)
             error = abs(angle - correct_answer)
-            total_error += error
-            pass_fail_string = "PASS" if error == 0 else "FAIL"
-            print(f"{filename} -- theta: {angle} -- correct_answer: {correct_answer} -- error: {error} -- {pass_fail_string}")
-
+            results[0][img_index] = angle
+            results[1][img_index] = error
+        
+        print(f"results = {results[0]}")
+        print(f"errors = {results[1]}")
+        print(f"Total error: {np.sum(results[1])}")
     # Write code to process the image
     # Write your code to calculate the angle and obtain the result as a list predAngles
     # Calculate and provide the error in predicting the angle for each image
